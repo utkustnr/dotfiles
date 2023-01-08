3548eb174c02e83ace7cabacde5d074b40e1a0a1
Utku <utkuustuner@proton.me>
2023-01-08 05:38:35 +0300

    Sort package list

--- ubuntu/ready-distro.sh
+++ ubuntu/ready-distro.sh
@@ -11,7 +11,7 @@ echo -e '	PATH="$HOME/platform-tools:$PATH"' >> ~/.profile
 echo -e 'fi' >> ~/.profile
 echo -e '\n' >> ~/.profile
 source ~/.profile
-sudo apt install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf libxml2 lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev imagemagick git lunzip lzop schedtool squashfs-tools xsltproc xattr zip zlib1g-dev openjdk-8-jdk python3 python-is-python3 perl xmlstarlet virtualenv xz-utils rr jq simg2img libncurses5 pngcrush lib32ncurses5-dev git-lfs libxml2 openjdk-11-jdk-headless -y
+sudo apt install bc bison build-essential curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick jq lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lunzip lzop openjdk-11-jdk-headless openjdk-8-jdk perl pngcrush python-is-python3 python3 rr rsync schedtool simg2img squashfs-tools virtualenv xattr xmlstarlet xsltproc xz-utils zip zlib1g-dev  -y
 mkdir -p ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
